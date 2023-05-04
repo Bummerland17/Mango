16fa68a9df22dad706ca052207c772dce5e9906e
Cindy Meyer <olcowboy21@gmail.com>
2023-05-04 12:15:20 -0600

    git add "vite.config.ts" git commit -m "Added images to the project and updated Home.tsx" git push origin master

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,12 @@ export default defineConfig({
       external: ['path-to-external-module', 'shardar-tarikul-islam-2vq33LK8bZA-unsplash.jpg', 'sam-hojati-lw8GflbJwLc-unsplash.jpg', 'heather-gill-ICVexvZjuC0-unsplash.jpg'],
     },
   },
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:4000',
+    },
+  },
 });
+
